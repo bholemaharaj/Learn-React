04316f5d7cdd675066e0a50d82940440ab88b0bc
bholemaharaj <bholemaharaj@hotmail.com>
2026-03-05 23:03:32 -0500

    Initialize Vite React Essentials project with basic setup
    - Created package.json with dependencies and scripts for development, build, and linting.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling the application layout and components.
    - Implemented App.jsx as the main application component, including a Welcome component.
    - Added Welcome component to greet users.
    - Included React logo SVG in assets directory.
    - Established global styles in index.css for consistent theming and layout.
    - Set up main.jsx to render the App component within a StrictMode.
    - Configured Vite with a basic setup in vite.config.js, integrating React plugin with Babel support.

---
+++ react-essentials/vite-react-essentials/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Welcome } from './Components/Welcome';
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1>vite-react-essentials</h1>
+      <Welcome name="Salil"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
